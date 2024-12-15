a3b813077623e3196bc4ad98c6ed5c3ee73140a5
Tanishq2903 <maharshitanishq@gmail.com>
2024-12-15 15:02:39 +0530

    check for no consecutive and first and last operator removed exit renamed some variables populated errors well fixed some minor issues

--- Calculator.c
+++ Calculator.c
@@ -1,95 +1,117 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
-//again raising pull request
-
-int pre(char operator){
-    if(operator == '+' || operator == '-')return 1;
-    else return 2;
-}
-int calculations(int num , int num1 , char operator){
-    int ans = 0;
-    if(operator == '+'){
-        ans = num + num1;
-    }
-    else if(operator == '-'){
-        ans = num - num1;
-    }
-     else if(operator == '*'){
-        ans = num * num1;
-    } else if(operator == '/'){
-       if(num1 != 0){
-         ans = num / num1;}
-       else{
-        printf("error:Division by zero");
-        exit(1);
-       }
-    }
-    return ans;
+int precedence(char operator)
+{
+    if (operator== '+' || operator== '-')
+        return 1;
+    else
+        return 2;
 }
 
-//again raising pull request
 
-int cal(char *exp){
-int ans= 0;
-int number = 0;
-int numbersarray[100];
-char operatorarray[100];
-int n = 0;
-int o = -1;
-int i = 0;
-while(i < strlen(exp)){
-    if(isdigit(exp[i])){
-        int num = 0;
-        while(i < strlen(exp) && isdigit(exp[i])){
-            num = num*10 + (exp[i]-'0');
-            i++;
+int calculations(int num, int num1, char operator)
+{
+    
+    if (operator== '+')
+    {
+        return num + num1;
+    }
+    else if (operator== '-')
+    {
+        return num - num1;
+    }
+    else if (operator== '*')
+    {
+        return num * num1;
+    }
+    else if (operator== '/')
+    {
+        if (num1 != 0)
+        {
+           return  num / num1;
         }
-        numbersarray[n++] = num;
-
-    } //again raising pull request
-    else if(exp[i] == '+' || exp[i] == '-' || exp[i] == '*' || exp[i] == '/'  ){
-        if(o >= 0 && pre(exp[i]) <= pre(operatorarray[o]) ){
-            ans = calculations(numbersarray[n-2],numbersarray[n-1],operatorarray[o]);
-            numbersarray[n-2] = ans;
-            n--;
-            o--;
+        else
+        {
+            printf("error:Division by zero");
+            return -1;
         }
-        operatorarray[++o] = exp[i];
-        i++;
-    }else if(exp[i] == ' '){
-        i++;
     }
-    else {
-        printf("Please enter valid string");
-        exit(1);
-    }
-
-
-}//again raising pull request
-
-
-while(o >= 0){
-    ans = calculations(numbersarray[n-2],numbersarray[n-1],operatorarray[o]);
-            numbersarray[n-2] = ans;
-            n--;
-            o--;
-}
-return numbersarray[0];
-
-
-}
-
-//again raising pull request
-
-int main(){
-    char exp[200];
-    printf("Enter the string :");
-    scanf("%[^\n]s",exp);//this %[^\n]s means input is taken till new line
-    int result = cal(exp);
-    printf("%d",result);
     return 0;
 }
-//again raising pull request
+
+int calculateString(char *expression)
+{
+    int ans = 0;
+    int number = 0;
+    int numbersarray[100];
+    char operatorarray[100];
+    int numtop = 0;
+    int operatortop = -1;
+    int i = 0;
+    while (i < strlen(expression))
+    {
+        if (isdigit(expression[i]))
+        {
+            int num = 0;
+            while (i < strlen(expression) && isdigit(expression[i]))
+            {
+                num = num * 10 + (expression[i] - '0');
+                i++;
+            }
+            numbersarray[numtop++] = num;
+        }
+        else if (expression[i] == '+' || expression[i] == '-' || expression[i] == '*' || expression[i] == '/')
+        {
+            if (i == 0 || !isdigit(expression[i-1]) )
+            {
+                printf("invalid string");
+                return -1;
+            }
+            while (operatortop >= 0 && precedence(expression[i]) <= precedence(operatorarray[operatortop]))
+            {
+                ans = calculations(numbersarray[numtop - 2], numbersarray[numtop - 1], operatorarray[operatortop]);
+                if(ans == -1)return -1;
+                numbersarray[numtop - 2] = ans;
+                numtop--;
+                operatortop--;
+            }
+            operatorarray[++operatortop] = expression[i];
+            i++;
+        }
+        else if (expression[i] == ' ')
+        {
+            i++;
+        }
+        else
+        {
+            printf("Please enter valid string");
+            return -1;
+        }
+    }
+
+    while (operatortop >= 0)
+    {
+        ans = calculations(numbersarray[numtop - 2], numbersarray[numtop - 1], operatorarray[operatortop]);
+        if(ans == -1)return -1;
+        numbersarray[numtop - 2] = ans;
+        numtop--;
+        operatortop--;
+    }
+    return numbersarray[0];
+}
+
+int main()
+{
+    char expression[200];
+    printf("Enter the string :");
+    scanf("%[^\n]s", expression); // this %[^\n]s means input is taken till new line
+    int result = calculateString(expression);
+    if(result == -1){
+        printf("some error occured");
+    }
+    else printf("result: %d",result);
+    return 0;
+}
